65b50f0559d58c4b365a76b4ae07794474d38039
pedrolucas7i <pedrolucas.core7i@gmail.com>
2024-10-12 11:51:56 +0100

    migration to Linux

--- 001/assembly.asm
+++ 001/assembly.asm
@@ -1,7 +1,9 @@
 bits 64
 
+section .text           ; Declare the code section
 global assembly
+
 assembly:
-  mov eax, 0x11223344
-  mov ax,  0xaabb
-  ret
+    mov eax, 0x11223344  ; Move a constant into eax
+    mov ax,  0xaabb      ; Move a constant into ax (lower 16 bits of eax)
+    ret                  ; Return
